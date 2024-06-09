eb43177f9707a9c201bc2cc4b1a20d418ccbffc3
Patternscompany <pandureddypatterns@gmail.com>
2024-06-09 23:18:23 +0530

    pp

--- seoAudit.js
+++ seoAudit.js
@@ -1,21 +1,21 @@
-// seoAudit.js
-
 const puppeteer = require('puppeteer');
 const path = require('path'); 
+const fs = require('fs');
 
 async function performAudit(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-      // Record the start time before navigating to the URL
-      const startTime = Date.now();
+    // Record the start time before navigating to the URL
+    const startTime = Date.now();
+
     try {
-        await page.goto(url, { waitUntil: 'networkidle2',timeout: 100000  });
- 
-   
-    
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
+
+        // Wait for the body element to be loaded
+        await page.waitForSelector('body', { timeout: 0 });
         const title = await page.title();
         const titleLen = title.length;
-        
+
         // Check the title length and add the appropriate symbol
         let symbolHTML = '';
         if (titleLen >= 10 && titleLen <= 70) {
@@ -23,13 +23,39 @@ async function performAudit(url) {
         } else {
             symbolHTML = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         }
-        
+
         // Append the symbol to the title
         const titleWithSymbol = `${title} ${symbolHTML}`;
-        
 
-          // Calculate the time taken to load the webpage
-    const loadTime = Date.now() - startTime;
+        // Calculate the time taken to load the webpage
+        const loadTime = Date.now() - startTime;
+
+        const businessData = await page.evaluate(() => {
+            const businessName = document.querySelector('selector-for-business-name')?.innerText;
+            const address = document.querySelector('selector-for-address')?.innerText;
+            const phone = document.querySelector('selector-for-phone')?.innerText;
+            const site = document.querySelector('selector-for-website')?.href;
+            const rating = document.querySelector('selector-for-rating')?.innerText;
+            const reviewsCount = document.querySelector('selector-for-reviews-count')?.innerText;
+
+            return {
+                businessName,
+                address,
+                phone,
+                site,
+                rating,
+                reviewsCount
+            };
+        });
+        console.log(businessData);
+
+
+
+
+
+
+
+
         const h1Tags = await page.evaluate(() => {
             const h1Elements = document.querySelectorAll('h1');
             const h1Content = [];
@@ -43,71 +69,60 @@ async function performAudit(url) {
         });
 
         let wordCount;
-  let wordCountWithSymbol;
-  try {
-    const textContent = await page.evaluate(() => {
-      return document.body.innerText;
-    });
+        let wordCountWithSymbol;
+        try {
+            const textContent = await page.evaluate(() => {
+                return document.body.innerText;
+            });
 
-    // Count the words
-    wordCount = textContent.split(/\s+/).filter(word => word.length > 0).length;
+            // Count the words
+            wordCount = textContent.split(/\s+/).filter(word => word.length > 0).length;
 
-    // Check the word count and add the appropriate symbol
-    let wordSymbolHTML = '';
-    if (wordCount >= 300) { // Adjust this threshold as needed
-      wordSymbolHTML = '<i class="fa fa-check" style="font-size:24px;color:green"></i>';
-    } else {
-      wordSymbolHTML = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
-    }
+            // Check the word count and add the appropriate symbol
+            let wordSymbolHTML = '';
+            if (wordCount >= 300) { // Adjust this threshold as needed
+                wordSymbolHTML = '<i class="fa fa-check" style="font-size:24px;color:green"></i>';
+            } else {
+                wordSymbolHTML = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
+            }
 
-    // Append the symbol to the word count
-    wordCountWithSymbol = `Word Count: ${wordCount} ${wordSymbolHTML}`;
-  } catch (error) {
-    wordCountWithSymbol = 'Word Count: <i class="fa fa-close" style="font-size:24px;color:red"></i>';
-  }
+            // Append the symbol to the word count
+            wordCountWithSymbol = `Word Count: ${wordCount} ${wordSymbolHTML}`;
+        } catch (error) {
+            wordCountWithSymbol = 'Word Count: <i class="fa fa-close" style="font-size:24px;color:red"></i>';
+        }
 
-        
-
-        // Get the meta description
-        // const metaDescription = await page.$eval('meta[name="description"]', element => element.content);
-  
-        let metaDescription;
+        let metaDescription = '';
+        let metaDescriptionLength = 0;
         try {
             metaDescription = await page.$eval('meta[name="description"]', element => element ? element.content : '');
-            
+
             // Check the meta description length and add the appropriate symbol
             let metaSymbolHTML = '';
-            if (metaDescription.length >= 70 && metaDescription.length <= 160) {
+            metaDescriptionLength = metaDescription.length;
+            if (metaDescriptionLength >= 70 && metaDescriptionLength <= 160) {
                 metaSymbolHTML = '<i class="fa fa-check" style="font-size:24px;color:green"></i>';
             } else {
                 metaSymbolHTML = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
             }
-        
+
             // Append the symbol to the meta description
             metaDescription += metaSymbolHTML;
         } catch (error) {
-            // metaDescription = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
+            metaDescription = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         }
-        
-
-
 
         let canonical;
         try {
-            // canonical = await page.$eval('link[rel="canonical"]',element => element.href);
             canonical = await page.$eval('link[rel="canonical"]', element => element.href);
             // If canonical link is found, add the check icon with green color
             canonical += ' <i class="fa fa-check" style="font-size:24px;color:green"></i>';
-        
         } catch (error) {
-       
             canonical = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         }
 
-
         let googleAnalyticsStatus;
         try {
-            // Evaluate the Google Analytics status on the page
             googleAnalyticsStatus = await page.evaluate(() => {
                 function checkGoogleAnalytics() {
                     const scripts = document.getElementsByTagName('script');
@@ -118,7 +133,7 @@ async function performAudit(url) {
                         /https:\/\/www\.googletagmanager\.com\/gtm\.js\?id=GTM-[A-Z0-9]+/,
                         /https:\/\/www\.googletagmanager\.com\/gtag\/js\?id=AW-[A-Z0-9]+/
                     ];
-        
+
                     for (let i = 0; i < scripts.length; i++) {
                         const src = scripts[i].src;
                         for (let j = 0; j < gaPatterns.length; j++) {
@@ -129,137 +144,123 @@ async function performAudit(url) {
                     }
                     return false;
                 }
-        
+
                 return checkGoogleAnalytics();
             });
-        
-            // If Google Analytics is found, add the check icon with green color
+
             if (googleAnalyticsStatus) {
                 googleAnalyticsStatus = '<i class="fa fa-check" style="font-size:24px;color:green"></i>';
             } else {
                 googleAnalyticsStatus = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
             }
-        
+
         } catch (error) {
             googleAnalyticsStatus = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         }
-        
-    
-        
-        
 
-        // Evaluate the function in the context of the page to count images
         const imageCount = await page.evaluate(() => {
             return document.querySelectorAll('img').length;
         });
 
-        let robotsTxtUrl='<i class="fa fa-close" style="font-size:24px;color:red"></i>';
+        let imageAudits;
+        try {
+            imageAudits = await page.evaluate(() => {
+                const images = document.querySelectorAll('img');
+                const results = [];
+
+                images.forEach(image => {
+                    const alt = image.getAttribute('alt');
+                    if (!alt || alt.trim() === '') {
+                        results.push({
+                            src: image.getAttribute('src'),
+                            alt: alt || '[MISSING]',
+                            error: 'Alt text is missing or empty',
+                            symbol: '<i class="fa fa-close" style="font-size:24px;color:red"></i>'
+                        });
+                    } else {
+                        results.push({
+                            src: image.getAttribute('src'),
+                            alt: alt,
+                            error: null,
+                            symbol: '<i class="fa fa-check" style="font-size:24px;color:green"></i>'
+                        });
+                    }
+                });
+
+                return results;
+            });
+        } catch (error) {
+            console.error('Error extracting image audits:', error);
+        }
+
+        // Take screenshots of the home page in both desktop and mobile views
+        const screenshotDir = path.resolve('public', 'screenshots');
+        if (!fs.existsSync(screenshotDir)) {
+            fs.mkdirSync(screenshotDir, { recursive: true });
+        }
+        // Desktop screenshot
+        // Revert to desktop view for further analysis if needed
+        await page.setViewport({ width: 1920, height: 1080 });
+        const desktopScreenshotPath = path.resolve(screenshotDir, 'desktop_screenshot.png');
+        await page.screenshot({ path: desktopScreenshotPath });
+        const desktopRelativePath = `screenshots/desktop_screenshot.png`;
+
+        // Mobile screenshot
+        await page.setViewport({ width: 375, height: 667, isMobile: true });
+        const mobileScreenshotPath = path.resolve(screenshotDir, 'mobile_screenshot.png');
+        await page.screenshot({ path: mobileScreenshotPath });
+        const mobileRelativePath = `screenshots/mobile_screenshot.png`;
+
+        //robotsfile
+        let robotsTxtUrl = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         try {
             const robotsTxtResponse = await page.goto(`${url}/robots.txt`);
             if (robotsTxtResponse && robotsTxtResponse.ok()) {
-                robotsTxtUrl = `${url}robots.txt  <i class="fa fa-check" style="font-size:24px;color:green"></i>`;
-
+                robotsTxtUrl = `${url}/robots.txt  <i class="fa fa-check" style="font-size:24px;color:green"></i>`;
             }
-
         } catch (error) {
-            // Handle errors when ;navigating to robots.txt
             console.error('Error navigating to robots.txt:', error);
         }
-        let sitemapUrl='<i class="fa fa-close" style="font-size:24px;color:red"></i>';
+//sitemap.xml
+        let sitemapUrl = '<i class="fa fa-close" style="font-size:24px;color:red"></i>';
         try {
             const sitemapResponse = await page.goto(`${url}/sitemap.xml`);
             if (sitemapResponse && sitemapResponse.ok()) {
-                sitemapUrl = `${url}sitemap.xml  <i class="fa fa-check" style="font-size:24px;color:green"></i>`;
-
+                sitemapUrl = `${url}/sitemap.xml  <i class="fa fa-check" style="font-size:24px;color:green"></i>`;
             }
-
         } catch (error) {
-            // Handle errors when ;navigating to robots.txt
             console.error('Error navigating to sitemap.xml:', error);
         }
-      
 
-        
-
-
-
-        // const robotsTxtExists = robotsTxtResponse && robotsTxtResponse.ok();
-
-
-      // Evaluate the function in the context of the page to audit images
-     // Extract the image audits
-  let imageAudits;
-  try {
-    imageAudits = await page.evaluate(() => {
-      const images = document.querySelectorAll('img');
-      const results = [];
-
-      images.forEach(image => {
-        const alt = image.getAttribute('alt');
-        if (!alt || alt.trim() === '') {
-          results.push({
-            src: image.getAttribute('src'),
-            alt: alt || '[MISSING]',
-            error: 'Alt text is missing or empty',
-            symbol: '<i class="fa fa-close" style="font-size:24px;color:red"></i>'
-          });
-        } else {
-          results.push({
-            src: image.getAttribute('src'),
-            alt: alt,
-            error: null,
-            symbol: '<i class="fa fa-check" style="font-size:24px;color:green"></i>'
-          });
-        }
-      });
-
-      return results;
-    });
-  } catch (error) {
-    console.error('Error extracting image audits:', error);
-  }
-
-
-// Take a screenshot
-// Resolve the absolute file path for saving the screenshot
-const screenshotPath = path.resolve('screenshot.png');
-const filename = path.basename(screenshotPath);
-
-const relativePath =  filename;
-await page.screenshot({ path: screenshotPath });
-       
-
-   
         return {
-            url:url,
-            
+            url: url,
             title: titleWithSymbol,
-            titleLen:titleLen,
-            loadtime:loadTime,
-            h1Content:h1Tags.content,
-         
-            wordCountWithSymbol:wordCountWithSymbol,
-            h1Count:h1Tags.count,
+            titleLen: titleLen,
+            loadtime: loadTime,
+            // businessData: businessData,
+            h1Content: h1Tags.content,
+            wordCountWithSymbol: wordCountWithSymbol,
+            h1Count: h1Tags.count,
             metaDescription: metaDescription,
-            metaDescriptionLength: metaDescription.length,
-            imagesC:imageCount,
-            imageA:imageAudits.length,
-            screenshot: relativePath,
-            robotsTxtExists:robotsTxtUrl,
-            canonical:canonical,
-            sitemapUrl:sitemapUrl,
-            googleAnalyticsStatus:googleAnalyticsStatus
+            metaDescriptionLength: metaDescriptionLength,
+            imagesC: imageCount,
+            imageA: imageAudits.length,
+            mobileRelativePath:mobileRelativePath,
+            desktopRelativePath:desktopRelativePath,
+            robotsTxtExists: robotsTxtUrl,
+            canonical: canonical,
+            sitemapUrl: sitemapUrl,
+            googleAnalyticsStatus: googleAnalyticsStatus
         };
 
-        
     } catch (error) {
         console.error('Error performing audit:', error);
         console.error('Error:', error);
-       // Return -1 to indicate error
+        // Return -1 to indicate error
         await browser.close();
         throw error; // Rethrow the error to be caught in the calling code
-    
-    }finally {
+
+    } finally {
         await browser.close();
     }
 }
